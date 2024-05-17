79ebb3c07e07da7d846bda6a786154cc7d98542a
raveeen01 <147915446+raveeen01@users.noreply.github.com>
2024-05-17 15:16:12 +0800

    crop_streamlit.py

--- crop_streamlit.py
+++ crop_streamlit.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1hQWTXaQg7gJGQsRb67srNqbNxlPXzAV6
 """
 
-pip install -q streamlit
-
 import streamlit as st
 import tensorflow as tf
 
